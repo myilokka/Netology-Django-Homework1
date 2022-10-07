be3d80918e69151c62276f8f7d99d07bf819024b
В.Б <rumpeldumpel03@gmail.com>
2022-10-07 23:27:07 +0800

    homework1 is done

---
+++ app/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    path = os.environ.get('path')
+    dirs = f'Содержимое рабочей директории {path} - {os.listdir(path)}.'
+    return HttpResponse(dirs)
